10a4dc2de6fed2a25016468e9b0243e401e24046
sats17 <satishkumbhar9999@gmail.com>
2021-04-25 15:12:34 +0530

    Added terminated opertations

--- reactivestreams-assignments/src/main/java/com/github/sats17/subscriptions/DataSubscription.java
+++ reactivestreams-assignments/src/main/java/com/github/sats17/subscriptions/DataSubscription.java
@@ -3,6 +3,7 @@ package com.github.sats17.subscriptions;
 import java.util.Iterator;
 import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.Flow.Subscription;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Subscription class between publisher and subscriber, and class contains methods will provide data on demands
@@ -14,6 +15,7 @@ public class DataSubscription implements Subscription {
 
 	private final Subscriber<? super Integer> dataSubscriber;
 	private Iterator<Integer> dataIterator;
+	private AtomicBoolean isTerminated = new AtomicBoolean(false);
 
 	public DataSubscription(Subscriber<? super Integer> subscriber, Iterator<Integer> iterator) {
 		this.dataSubscriber = subscriber;
@@ -27,18 +29,21 @@ public class DataSubscription implements Subscription {
 			dataSubscriber.onError(new IllegalArgumentException("Enter proper values for request"));
 		}
 		// Data will be pushed to subscriber here
-		for (int demand = 0; demand < n && dataIterator.hasNext(); demand++) {
+		for (int demand = 0; demand < n && dataIterator.hasNext() && !isTerminated.get(); demand++) {
 			dataSubscriber.onNext(dataIterator.next());
 		}
 		// If iterator has no data to provide then will notify subscriber with onComplete method
-		if (!dataIterator.hasNext()) {
+		if (!dataIterator.hasNext() && !isTerminated.getAndSet(true)) {
 			dataSubscriber.onComplete();
 		}
 	}
 
+	/**
+	 * Once cancel method calls, no stream data will be pushed
+	 */
 	@Override
 	public void cancel() {
-		dataSubscriber.onComplete();
+		isTerminated.set(true);
 	}
 
 }
