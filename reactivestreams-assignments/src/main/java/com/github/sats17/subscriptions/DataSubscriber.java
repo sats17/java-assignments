60069b206de412d362ccfeb907074825d03a51f0
sats17 <satishkumbhar9999@gmail.com>
2021-04-25 19:39:37 +0530

    Updated subscriber and added tck test

--- reactivestreams-assignments/src/main/java/com/github/sats17/subscriptions/DataSubscriber.java
+++ reactivestreams-assignments/src/main/java/com/github/sats17/subscriptions/DataSubscriber.java
@@ -10,41 +10,50 @@ import java.util.concurrent.Flow.Subscription;
  * @author sats17
  *
  */
-public class DataSubscriber {
+public class DataSubscriber implements Subscriber<Integer>{
+	
+	Integer request;
+	
+	Subscription subscription;
+	
+	public DataSubscriber(Integer request) {
+		this.request = request;
+	}
+	
+	/**
+	 * Subscribing to publisher and publisher will return data whatever we requested
+	 */
+	@Override
+	public void onSubscribe(Subscription subscription) {
+		this.subscription = subscription;
+		System.out.println("Requesting first "+this.request+" elements to publisher");
+		subscription.request(this.request); // Requesting number of first data that have to publisher will be given
+	}
+
+	@Override
+	public void onNext(Integer item) {
+		// Print whatever data that DataPublisher publishes
+		System.out.println("Data receives from publisher " + item);
+	}
+
+	@Override
+	public void onError(Throwable throwable) {
+		// Any error occurs that will handle here
+		System.out.println(throwable.getMessage());
+	}
+
+	@Override
+	public void onComplete() {
+		this.subscription.cancel();
+		// If publisher is done with his data pushing that he have, then he will call this method
+		System.out.println("We received all data from our publisher");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Started Reactive Streams!\n");
-
 		// Creating object of publisher class and passing subscriber to publisher object
-		new DataPublisher().subscribe(new Subscriber<Integer>() {
-
-			/**
-			 * Subscribing to publisher and publisher will return data whatever we requested
-			 */
-			@Override
-			public void onSubscribe(Subscription subscription) {
-				int request = 8;
-				System.out.println("Requested first "+request+" elements to publisher");
-				subscription.request(request); // Requesting number of first data that have to publisher will be given
-			}
-
-			@Override
-			public void onNext(Integer item) {
-				// Print whatever data that DataPublisher publishes
-				System.out.println("Data receives from publisher " + item);
-			}
-
-			@Override
-			public void onError(Throwable throwable) {
-				// Any error occurs that will handle here
-				System.out.println(throwable.getMessage());
-			}
-
-			@Override
-			public void onComplete() {
-				// If publisher is done with his data pushing that he have, then he will call this method
-				System.out.println("We received all data from our publisher");
-			}
-		});
+		new DataPublisher().subscribe(new DataSubscriber(1));
+		new DataPublisher().subscribe(new DataSubscriber(20));
 	}
 
 }
