20aee7f1dab4fc5e77431b85f36004cbb2866fe9
sats17 <satishkumbhar9999@gmail.com>
2021-09-30 21:18:49 +0530

    Added information about FI

--- streams-assignments/src/main/java/com/github/sats17/streams/examples/TransactionOperations.java
+++ streams-assignments/src/main/java/com/github/sats17/streams/examples/TransactionOperations.java
@@ -8,18 +8,31 @@ import com.github.sats17.streams.model.Transaction;
 
 public class TransactionOperations {
 
-	public static Function<ArrayList<Transaction>, ArrayList<Transaction>> sortByYear = transactions -> {
-		
+	// below variable is function and class implementation of functional interface nothing but syntatical sugar of 
+	// below sortByYear2 variable implementation
+	public static Function<ArrayList<Transaction>, ArrayList<Transaction>> sortByYear = (transactions) -> {
+
 		return transactions.stream()
-					.filter(transaction -> transaction.getYear() == 2011)
-					.sorted((t1,t2) -> {
-						return Integer.compare(t2.getValue(),t1.getValue());
-					})
-					.collect(Collectors.toCollection(ArrayList::new));
-		
-		
+						   .filter(transaction -> transaction.getYear() == 2011)
+						   .sorted((t1, t2) -> {
+							   return Integer.compare(t2.getValue(), t1.getValue());
+						   }).collect(Collectors.toCollection(ArrayList::new));
+
 	};
 	
-	
+	public static test sortByYear2 = new test();
+
+}
+
+class test implements Function<ArrayList<Transaction>, ArrayList<Transaction>> {
+
+	@Override
+	public ArrayList<Transaction> apply(ArrayList<Transaction> t) {
+		return t.stream()
+				   .filter(transaction -> transaction.getYear() == 2011)
+				   .sorted((t1, t2) -> {
+					   return Integer.compare(t2.getValue(), t1.getValue());
+				   }).collect(Collectors.toCollection(ArrayList::new));
+	}
 	
 }
