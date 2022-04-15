95ca5af3389fb23bd55caab0619a8bdff6187ad5
sats17 <satishkumbhar9999@gmail.com>
2022-04-15 20:49:22 +0530

    Added stream operations

--- streams-assignments/src/main/java/com/github/sats17/streams/examples/FruitSort.java
+++ streams-assignments/src/main/java/com/github/sats17/streams/examples/FruitSort.java
@@ -10,65 +10,96 @@ import java.util.stream.Collectors;
 
 import com.github.sats17.streams.model.Fruit;
 
+
 public class FruitSort {
+	
+	private static final int inflationAmount = 2;
 	
 	
 	/*
 	 * Fiters fruit having calories less than 100, then sort fruits in ascending order by comparing calories and price(If calories is same)
 	 */
-	public static Function<List<Fruit>, List<Fruit>> sort = (List<Fruit> fruits) -> {
+	public static Function<List<Fruit>, List<Fruit>> sortByCalories = (List<Fruit> fruits) -> {
 		
 		return fruits.stream()
-			  .filter(fruit -> fruit.getCalories() < 100 )
-			  .sorted((f1,f2) -> {
+				.filter(fruit -> {
+					System.out.println("From first filter");
+					return true;
+				}).
+				map(f -> {
+					System.out.println("From map");
+					f.setCalories(f.getCalories() * inflationAmount);
+					System.out.println(f);
+					return f;
+				})
+				.filter(fruit -> {
+					System.out.println("From second filter");
+					return true;
+				})
+			  .sorted((f1,f2) -> { 
+				  System.out.println("From sorted");
+				  System.out.println(f1);
+				  System.out.println(f2);
 				  int result = Integer.compare(f2.getCalories(), f1.getCalories());
-				  if(result == 0)
-					  return Integer.compare(f2.getPrice(),f1.getPrice());
+				  System.out.println("Sort result "+result);
+//				  if(result == 0)
+//					  return Integer.compare(f2.getPrice(),f1.getPrice());
 				return result;
-			  })
+			  }).
+			  filter(fruit -> {
+					System.out.println("From third filter");
+					return true;
+				}).
+			  filter(fruit -> {
+					System.out.println("From Fourth filter");
+					return true;
+				})
 			  .collect(Collectors.toCollection(ArrayList::new));
+	};
+	
+	private static Function<List<Fruit>, List<Fruit>> sortByName = (List<Fruit> fruit) -> {
+		return fruit.stream()
+				.sorted((f1, f2) -> {
+					return f1.getName().compareTo(f2.getName());
+				}).collect(Collectors.toList());
 	};
 
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		List<Fruit> data = new ArrayList<Fruit>();
-		ArrayList<String> test = new ArrayList<>();
-		test.add("test");
-		test.add("abes");
-		Fruit f0 = new Fruit("Chiku", 10000, 2, "Yellow"); 
+		
+		Fruit f0 = new Fruit("Chiku", 10000, 2, "grey"); 
 		data.add(f0);
 		Fruit f1 = new Fruit("Banana", 800, 20000, "Yellow");
 		Fruit f2 = new Fruit("Apple", 160, 200, "Red");
-		Fruit f3 = new Fruit("Pineapple", 170, 250, "Yellow");
+		Fruit f3 = new Fruit("Pineapple", 170, 250, "yellow");
 		data.add(f1);
 		data.add(f2);
 		data.add(f3);
-//		for(Object f: FruitSort.sort.apply(data).toArray()) {
-//			System.out.println(f);
-//		}
-		Comparator<Fruit> compareString = (s1, s2) -> {
-			System.out.println("Before this Comparing");
-			System.out.println("data is "+data);
-			System.out.println("below values");
-			System.out.println(s1);
-			System.out.println(s2);
-			System.out.println("\n");
-			if(s1.getCalories() == s2.getCalories()) {
-				return 0;
-			}
-			if(s1.getCalories() > s2.getCalories()) {
-				return 1;
-			}
-			return -1;
-		};
-		System.out.println(data);
-		Collections.sort(data, compareString);
-		System.out.println("After sort");
-		System.out.println(data);
-//		test.forEach(f -> {
-//			System.out.println(f);
-//		});
-//		System.out.println(Integer.compare(1, 7));
+		
+		
+		
+		data.forEach(fruit -> System.out.println(fruit));
+		System.out.println("Data feeding done");
+		System.out.println("\n");
+		List<Fruit> result = FruitSort.sortByCalories.apply(data);
+		System.out.println("\n");
+		result.forEach(fruit -> System.out.println(fruit));
+		
+		// To check sort by name uncomment below code
+//		System.out.println("Sort by name started");
+//		FruitSort.sortByName.apply(data).forEach(fruit -> System.out.println(fruit));
+		
+		// Below one will thorw error because Fruit do not have comparable implemented
+		//data.stream().sorted().forEach(fruit -> System.out.println(fruit));
+		
+		
+		// Compare by color comparator written as a variable. To check uncomment below code
+//		System.out.println("Sort by color started");
+//		Comparator<Fruit> compareByColour = (fruit1, fruit2) -> {
+//			return fruit1.getColor().compareToIgnoreCase(fruit2.getColor());
+//		};
+//		data.stream().sorted(compareByColour).forEach(fruit -> System.out.println(fruit));
 	}
 
 }
