3fdce7b66050175326db0e19a5f3b16ff89803e4
sats17 <satishkumbhar9999@gmail.com>
2024-01-07 19:52:10 +0530

    Added optimised reverse array

--- problems-assignments/src/main/java/com/github/sats17/problems/assignments/RotateArray.java
+++ problems-assignments/src/main/java/com/github/sats17/problems/assignments/RotateArray.java
@@ -1,17 +1,38 @@
 package com.github.sats17.problems.assignments;
 
-import java.util.Arrays;
-
 public class RotateArray {
 
+	public int[] reverse(int[] arr, int startIndex, int endIndex) {
+		if (endIndex - startIndex == 1) {
+			int startIndexVal = arr[startIndex];
+			arr[startIndex] = arr[endIndex];
+			arr[endIndex] = startIndexVal;
+		} else {
+			int median = (startIndex + endIndex) / 2;
+			while (startIndex <= median) {
+				int startIndexVal = arr[startIndex];
+				arr[startIndex] = arr[endIndex];
+				arr[endIndex] = startIndexVal;
+				startIndex++;
+				endIndex--;
+			}
+		}
+		for (int k = 0; k < arr.length; k++) {
+			System.out.println("Reverse value " + arr[k]);
+		}
+		System.out.println("=================");
+		return arr;
+	}
+
 	/**
-	 * Time complexity = O(n), where n is size of array.
-	 * Space complexity = O(n * 2) where n is size of array
+	 * Time complexity = O(n), where n is size of array. Space complexity = O(n * 2)
+	 * where n is size of array
+	 * 
 	 * @param arr
 	 * @param rotateSteps
 	 */
 	public void simpleRotate(int[] arr, int rotateSteps) {
-		
+
 		int arrSize = arr.length;
 		int rotateStartIndex = arrSize - rotateSteps;
 		int newArrIndex = 0;
@@ -23,7 +44,7 @@ public class RotateArray {
 			newArrIndex++;
 			rotateStartIndexLoop++;
 		}
-		
+
 		// Fill remaining values to array. Remaining values are the start one.
 		int oldArrStartIndex = 0;
 		while (oldArrStartIndex < rotateStartIndex) {
@@ -32,17 +53,32 @@ public class RotateArray {
 			newArrIndex++;
 			oldArrStartIndex++;
 		}
-		
+
 		// Print new array
 		for (int k = 0; k < myIntArray.length; k++) {
 			System.out.println("Rotate value " + myIntArray[k]);
+		}
+	}
+	
+	public void optimizedRotate(int[] arr, int rotateSteps) {
+
+		int arrSize = arr.length - 1;
+		int medianIndex = (arrSize - rotateSteps);
+		int[] reversedArr = reverse(arr, 0, medianIndex );
+		int[] reverseArr2 = reverse(reversedArr, medianIndex + 1, arrSize);
+		int[] finalRotatedArr = reverse(reverseArr2, 0, arrSize);
+		// Print new array
+		for (int k = 0; k < finalRotatedArr.length; k++) {
+			System.out.println("Rotate value " + finalRotatedArr[k]);
 		}
 	}
 
 	public static void main(String[] args) {
 		int[] arr = new int[] { 2, 3, 4, 5, 6, 7, 8, 9 };
 		RotateArray ra = new RotateArray();
-		ra.simpleRotate(arr, 7);
+//		ra.simpleRotate(arr, 7);
+		ra.optimizedRotate(arr, 4);
+//		ra.reverse(arr, 4, 7);
 	}
 
 }
