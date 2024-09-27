47bd656fa5d007e46ddc0139469b5ce73294ab4c
sats17 <satishkumbhar9999@gmail.com>
2024-09-27 09:16:31 +0530

    Added generalized search for nested array with combination of binary search and iteration

--- problems-assignments/src/main/java/com/github/sats17/problems/assignments/strivers/SearchInSortedArray.java
+++ problems-assignments/src/main/java/com/github/sats17/problems/assignments/strivers/SearchInSortedArray.java
@@ -2,7 +2,7 @@ package com.github.sats17.problems.assignments.strivers;
 
 public class SearchInSortedArray {
 
-	public static boolean findIn2DArray(int[][] arr, int k) {
+	public static boolean findIn2DArrayWithFormula(int[][] arr, int k) {
 		int arrLen = arr.length;
 
 		
@@ -26,8 +26,42 @@ public class SearchInSortedArray {
 		return false;
 	}
 
+	
+	public static boolean findIn2DArrayWithBinarySearch(int[][] arr, int k) {
+		int arrLen = arr.length;
+		// Iterate each row
+		for(int i = 0; i < arrLen; i++) {
+			int left = 0;
+			int right = arr[i].length - 1;
+			// Find out if value present in row by checking first and last element. Since array is sorted
+			// We can do this.
+			if(arr[i][left] <= k && arr[i][right] >= k) {
+
+				// If value is present perform binary search on that row.
+				while(left <= right) {
+					int mid = left + (right - left) / 2;
+					
+					if (arr[i][mid] == k) {
+						System.out.println("Item found at index "+i+","+mid);
+						return true;
+					} else if(arr[i][mid]  > k) {
+						right = mid - 1;
+					} else {
+						left = mid + 1;
+					}
+				}
+			}
+			
+
+		}
+		
+		
+		return false;
+	}
+
+	
 	public static void main(String[] args) {
-		int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6, 7 }, { 8, 9, 10, 11, 12 } };
 
 		// Printing the 2D array
 		for (int i = 0; i < matrix.length; i++) {
@@ -37,7 +71,7 @@ public class SearchInSortedArray {
 			System.out.println();
 		}
 		System.out.println("--------------------------------");
-		boolean ans = findIn2DArray(matrix, 19);
+		boolean ans = findIn2DArrayWithBinarySearch(matrix, 2);
 		System.out.println(ans);
 	}
 
