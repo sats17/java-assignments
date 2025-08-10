28e85134035103e66355842389e36887b655aa47
sats17 <satishkumbhar9999@gmail.com>
2025-08-10 21:21:33 +0530

    Added virtual thread example

---
+++ thread-assignments/src/main/java/com/github/sats17/virtualthread/VirtualThreadExample.java
@@ -0,0 +1,34 @@
+package com.github.sats17.virtualthread;
+
+// Java program to demonstrate exchange
+// Data between threads using scoped values
+import java.util.*;
+
+//Driver class
+public class VirtualThreadExample {
+    // main function
+    public static void main(String[] args) {
+        try {
+
+            // Initialization of thread
+            Thread.Builder builder = Thread.ofPlatform().name("Custom thread");
+
+            Runnable task = () -> {
+                System.out.println("Running thread");
+            };
+
+            Thread t = builder.start(task);
+
+            System.out.println("Thread t name: " + t.getName());
+
+            // Add a delay to allow the virtual thread to run
+            // Sleep for 1 second
+            Thread.sleep(1000);
+
+            // Wait for the thread to complete
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
