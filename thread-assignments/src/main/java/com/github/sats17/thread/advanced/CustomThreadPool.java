493a65b7949d45933108e50ea12917a39ab62a81
sats17 <satishkumbhar9999@gmail.com>
2026-08-09 12:43:03 +0530

    Updated thread pool

--- thread-assignments/src/main/java/com/github/sats17/thread/advanced/CustomThreadPool.java
+++ thread-assignments/src/main/java/com/github/sats17/thread/advanced/CustomThreadPool.java
@@ -10,6 +10,7 @@ public class CustomThreadPool {
     List<Thread> threads;
     BlockingQueue<Runnable> tasks;
     private static final Runnable POISON = () -> {}; // Special task that wake all threads to close
+    private volatile boolean shutDownAsked = false;
 
     public CustomThreadPool(int threads) {
         this.threads = new ArrayList<>(threads);
@@ -35,19 +36,26 @@ public class CustomThreadPool {
         }
     }
 
-    public void submit(Runnable runnable) throws InterruptedException {
+    public void execute(Runnable runnable) {
+        if(shutDownAsked) {
+            throw new IllegalStateException("Cannot accept new tasks as shutdown being asked.");
+        }
         tasks.add(runnable);
     }
     
     public void shutDown() {
         System.out.println("Shutdown asked, once all task completed threadpool will shutdown");
+        if(shutDownAsked) {
+            return;
+        }
+        shutDownAsked = true;
         for (int i = 0; i < threads.size(); i++) {
             tasks.add(POISON);
         }
     }
 
     static void main() throws InterruptedException {
-        CustomThreadPool customThreadPool = new CustomThreadPool(2);
+        CustomThreadPool customThreadPool = new CustomThreadPool(12);
         Runnable work = () ->  {
             System.out.println("Work started on thread: "+Thread.currentThread().getName());
             long sum = 0;
@@ -57,11 +65,15 @@ public class CustomThreadPool {
             System.out.println(sum);
             System.out.println("Work completed on thread: "+Thread.currentThread().getName());
         };
-        for(int i = 0; i < 5; i++) {
-            customThreadPool.submit(work);
+        for(int i = 0; i < 13; i++) {
+            if(i == 12) {
+                customThreadPool.shutDown();
+            }
+            customThreadPool.execute(work);
             System.out.println("Submitted work from "+Thread.currentThread().getName());
+
         }
-        Thread.sleep(20000);
-        customThreadPool.shutDown();
+//        Thread.sleep(10);
+//        customThreadPool.shutDown();
     }
 }
